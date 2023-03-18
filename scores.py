1648af3f970f241d6f8cc55033f70f27d97c46e1
DanielZh <haveacar.zhovnir@gmail.com>
2023-03-18 17:51:16 +0200

    added func high score in stats.py

--- scores.py
+++ scores.py
@@ -19,14 +19,15 @@ class Scores:
         self.score_rect.right = self.screen_rect.right -40
         self.score_rect.top = 20
 
+
+    def image_high_score(self):
+        """Rend text to graphic"""
+        self.high_score_image = self.font.render(str(self.stats.high_score), True, self.text_color, (0, 0, 0))
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.screen_rect.top + 20
+
     def show_score(self):
         """show score on screen"""
         self.screen.blit(self.score_img, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
-
-    def image_high_score(self):
-        """Rend text to graphic"""
-        self.high_score_image = self.font.render(str(self.stats.high_score),True, self.text_color, (0,0,0))
-        self.high_score_rect = self.high_score_image.get_rect()
-        self.high_score_rect.centerx = self.score_rect.centerx
-        self.high_score_rect.top = self.high_score_rect.top + 20
