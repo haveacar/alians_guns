003a1d6b19621aeb77216a5b5c1361f34e4f486f
DanielZh <haveacar.zhovnir@gmail.com>
2023-03-20 10:11:35 +0200

    added func request

--- database.py
+++ database.py
@@ -6,3 +6,29 @@ DATABASE_PASSWORD = "kGMMHFUyl6"
 DATABASE_NAME = "sql7606287"
 TABLE_NAME = 'users'
 
+mail_user = ''
+SQL_QUERY = f'SELECT * FROM {TABLE_NAME} WHERE mail="{mail_user}";'
+class Database:
+    """Database """
+    def __init__(self):
+
+        pass
+
+    def request(self, sql_req, result = False):
+        try:
+            with mysql.connector.connect(host=SQL_HOST,
+                                         user=DATABASE_USER,
+                                         password=DATABASE_PASSWORD,
+                                         database=DATABASE_NAME
+                                         ) as con:
+                # cursor object
+                cursor = con.cursor()
+                # execute query
+                cursor.execute(sql_req)
+                if result:
+                    query_result = cursor.fetchall()
+                    return query_result
+
+        except Exception as err:
+            print("Error: ", err)
+            return False
