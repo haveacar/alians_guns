f05370954114f25bc46f63c90839550e0bd4a317
DanielZh <haveacar.zhovnir@gmail.com>
2023-03-20 20:09:01 +0200

    added login push func

--- database.py
+++ database.py
@@ -13,7 +13,6 @@ SQL_QUERY = f'SELECT * FROM {TABLE_NAME} WHERE mail="{mail_user}";'
 # font
 FONT = ("Ariel", 18)
 
-
 """def request(sql_req, result = False):
     try:
         with mysql.connector.connect(host=SQL_HOST,
@@ -33,22 +32,22 @@ FONT = ("Ariel", 18)
         print("Error: ", err)
         return False"""
 
+
 class Login(Tk):
     """Class Login Page"""
+
     def __init__(self):
         super(Login, self).__init__()
-
 
         # set up window
         self.title("Login Page")
         self.geometry("600x300")
         self.resizable(False, False)
 
-        #Create labels and text input boxes for username and password
+        # Create labels and text input boxes for username and password
         self.username_label_email = Label(text="Email:", font=FONT)
 
         self.username_entry_email = Entry()
-
 
         self.username_label = Label(text="Username:", font=FONT)
         self.username_label.pack()
@@ -64,13 +63,39 @@ class Login(Tk):
         self.login_button.pack()
         self.sign_button = Button(text="Sign", font=FONT, width=3, command=self.sign_push)
         self.sign_button.pack()
-        self.reg_button = Button(text="Registrate", font=FONT, width=7)
-
+        self.reg_button = Button(text="Registrate", font=FONT, width=7, command=self.reg_push)
 
         self.mainloop()
 
     def login_push(self):
-        pass
+
+        # Retrieve username and password from text input boxes
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        # Query MySQL database to check if entered credentials are valid
+        try:
+            with mysql.connector.connect(host=SQL_HOST,
+                                         user=DATABASE_USER,
+                                         password=DATABASE_PASSWORD,
+                                         database=DATABASE_NAME
+                                         ) as con:
+                # cursor object
+                cursor = con.cursor()
+                # execute query
+                cursor.execute("SELECT password_hash FROM users WHERE username = %s", (username,))
+                result = cursor.fetchone()
+        except EXCEPTION as err:
+            messagebox.showerror("Error", f"Error: {err}")
+        else:
+            # Check if password matches password hash from database
+            if result and password == result[0]:
+                # Allow user to log in
+                messagebox.showinfo("Login Successful", "You have successfully logged in!")
+            else:
+                # Display error message if password is incorrect
+                messagebox.showerror("Login Error", "Invalid username or password.")
+
 
     def sign_push(self):
         """sign push button"""
@@ -79,7 +104,8 @@ class Login(Tk):
         self.sign_button.config(state="disabled")
         self.reg_button.pack(after=self.sign_button)
 
+    def reg_push(self):
+        pass
 
 
-login= Login()
-
+login = Login()
