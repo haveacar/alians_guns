c118b59d989750ea4a5bde076c3ad90049026abf
DanielZh <haveacar.zhovnir@gmail.com>
2023-03-20 10:18:49 +0200

    moved class request to func

--- database.py
+++ database.py
@@ -8,27 +8,23 @@ TABLE_NAME = 'users'
 
 mail_user = ''
 SQL_QUERY = f'SELECT * FROM {TABLE_NAME} WHERE mail="{mail_user}";'
-class Database:
-    """Database """
-    def __init__(self):
 
-        pass
 
-    def request(self, sql_req, result = False):
-        try:
-            with mysql.connector.connect(host=SQL_HOST,
+def request(sql_req, result = False):
+    try:
+        with mysql.connector.connect(host=SQL_HOST,
                                          user=DATABASE_USER,
                                          password=DATABASE_PASSWORD,
                                          database=DATABASE_NAME
                                          ) as con:
-                # cursor object
-                cursor = con.cursor()
-                # execute query
-                cursor.execute(sql_req)
-                if result:
-                    query_result = cursor.fetchall()
-                    return query_result
+            # cursor object
+            cursor = con.cursor()
+            # execute query
+            cursor.execute(sql_req)
+            if result:
+                query_result = cursor.fetchall()
+                return query_result
 
-        except Exception as err:
-            print("Error: ", err)
-            return False
+    except Exception as err:
+        print("Error: ", err)
+        return False
